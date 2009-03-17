f4c21f5efd103d584bd40c6a50c5dafe82aec19e
crazybilly <crazybilly@gmail.com>
2009-03-17 13:41:45 -0500

    Index done. Gonna have to do page.php for comments

--- theme/index.php
+++ theme/index.php
@@ -1,30 +1,43 @@
-<?php
+<?
 
 //grab header.php
 get_header();
 
-//start The Loop
-if (have_posts()) :
-   while (have_posts()) :
-      the_post(); 
-?>
-	
-<div class="location">
-	<h2>
-		<? //do individual pages? or write a function and just index? the_category ?>
+//start the Loop
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
+//Location div ?>
+<div class="location <? if (is_page() ) { echo 'hidden'; } ?> ">
 
-
-<? endwhile; else ?>
-
-	<p><? _e('No posts found.'); ?></p>
-
-<?endif; ?>
+	<h2><? /*function to write Location title based on page type*/ ?></h2>
+	<h3 id="search_string" class= " <? if (!is_search()) { echo 'hidden'; } ?> ">'<? the_search_query() //might also use get_search_query() ?>'</h3>
 
 </div>
 
+<div class="post">
+<?//Start the post?>
+
+<a href="<? the_permalink(); ?>"><?the_title ('<h2>','</h2'>);?></a>
+<?the_date  ('m.d.y', '<h4>','</h4>');?>
+
+	<div class="post-content">
+		<? the_content () ?>
+	</div>
+
+		<div class="post-meta">
+			Posted in <? the_category (', ') ?> | <a href="<? comments_link(); ?>"><? comments_number() ?></a>
+						<br>
+				<? the_tags ('Tags:', ', ',,); ?>
+		</div>
+
+<?//close the loop
+endwhile; else: ?>
+	<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+<?php endif; ?>
+
+</div>
+	
 <?
 get_sidebar();
 get_footer(); 
 ?>
-
--- theme/sidebar.php
+++ theme/sidebar.php
@@ -1,48 +1,56 @@
 <div class="sidebar">
-	<div id="sidebarleft">
-<? 
+<div id="sidebarleft">
+	<? 
 
-//if you're on the homepage
+	if (is_home ()) {
+		if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar(1) ) :
+			//use below function in a text thingy
+			?><h2>Recently</h2><?
 
-	?><h2>Recently</h2><?
+				query_posts('showposts=1');
+				global $more;
+				$more = 0; 
 
-	query_posts('showposts=1');
-		global $more;
-		$more = 0; 
-
-	//start the loop
-	if (have_posts()) :
-	   while (have_posts()) :
-    	  the_post(); 	
+				//start the loop
+				if (have_posts()) :
+				   while (have_posts()) :
+			    	  the_post(); 	
 	
-		/*doing it via the_content	
-		 *requires <!--more --> or whatever*/
-		the_content('Read more&#8230;');
+				/*doing it via the_content	
+				 *requires <!--more --> or whatever*/
+					  the_content('Read more&#8230;');
+		
+				//close the loop
+				endwhile; endif;
+			endif;
 
+	} else {
+		//#2 = sidebarleft for !home
+		if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar(2) ) :
 
-	//close the loop
-	endwhile; else: 
-	endif;
-
-
-//if you're not on the homepage
-	//get left sidebar
-	if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar(1) ) :
-
-	endif;
-
+		endif;
+		}
 ?>
 </div>
 
+<!--now for the right sidebar -->
+<div id="sidebarright"><?
 
-<div id="sidebarright">
-<?
+	if (is_home ()) {
+		if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar(3) ) :
+			?><h2>About</h2>
 
-	//get sidebar right
-	if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar(2) ) : 
+				<p>blogging at the intersection of postmodern Christianity and open source</p>
 
-	endif;
+		<?endif;
 
-?>
+	} else {
+		if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar(4) ) :
+
+		endif;} ?>
+
 </div>
+
+
 </div>
+
