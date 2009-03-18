b6e22af058c7d80b910fd7a4c1ef8f42cf42c659
crazybilly <crazybilly@gmail.com>
2009-03-18 16:54:46 -0500

    created files for comments

---
+++ theme/page.php
@@ -0,0 +1,49 @@
+<?
+
+//grab header.php
+get_header();
+
+//Location div ?>
+<div class="location <? if (is_page() ) { echo 'hidden'; } ?> ">
+
+	<h2><? echo location() ?></h2>
+	<h3 id="search_string" class= " <? if (!is_search()) { echo 'hidden'; } ?> ">'<? the_search_query() //might also use get_search_query() ?>'</h3>
+
+</div>
+<div class="content">
+
+<?
+//start the Loop
+if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+
+
+<div class="post">
+<?//Start the post?>
+
+<a href="<? the_permalink(); ?>"><?the_title('<h2>','</h2>');?></a>
+<?the_date  ('m.d.y', '<h4>','</h4>');?>
+
+	<div class="post-content">
+		<? the_content () ?>
+	</div>
+
+		<div class="post-meta">
+			Posted in <? the_category (', ') ?> | <a href="<? comments_link(); ?>"><? comments_number() ?></a>
+						<br>
+				<? the_tags('Tags:',', '); ?>
+		</div>
+
+<?//close the loop
+endwhile; else: ?>
+	<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+<?php endif; ?>
+
+</div>
+</div>
+</div>
+
+<?
+get_sidebar();
+get_footer(); 
+?>
