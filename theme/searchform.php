5a4c2783dd4081a157d400d126da9052ff650eb4
crazybilly <crazybilly@gmail.com>
2009-04-08 21:02:51 -0500

    search form tweaks

--- theme/searchform.php
+++ theme/searchform.php
@@ -2,7 +2,7 @@
 
 
 	<div>
-		<input type="text" value="" name="s" id="s" size="37" >
+		<input type="text" value="listen for..." name="s" id="s" size="37" onfocus="this.value=''">
 		<input type="submit" id="searchsubmit" value="Search" >
 	</div>
 </form>			
